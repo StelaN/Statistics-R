b851e5bb428a86b5d37f4e3934c6e725a5fa7d5a
Stela Novachkova <stelanovachkova@gmail.com>
2017-06-12 17:28:13 +0300

    added theory

--- week8.r
+++ week8.r
@@ -30,6 +30,13 @@ sd(x) #стандартно отклонение
 #тества дали две извадки имат еднаква дисперсия
 #функцията f() с префикси d,p,q,r за съответно плътност, разпределние, квантил и случайно разпределение(симулиране)
 
+#доверителен интервал
+#имаме случайна величина X, която е с някакво разпределение, но не знаем някой параметър tita. Искаме да оценим този параметър tita с някаква вероятност на достоверност (gama = 1 - alpha). Тогава gama = 1 - alpha = P(-z* < tita < z*) -> от -z* до z* е  доверителния интервал  
+#нормално разпределение -> z*=qnorm(1-alpha / 2) (защото е симетрично)
+#биномно разпределение, когато tita = p (не знаем каква е вероятността за успех) -> интервала е [p.shapka - z* SE, p.shapka + z* SE], където p.shapka е експериментално получената вероятност, SE = sqrt((1/n) * p.shapka * (1-p.shapka))
+#биномно разпределение, когато tita = p1 - p2 (искаме да оценим разликата във вероятностите за две случайно разпределни величини с големина n и m) -> интервала е [tita - z* SE, tita + z* SE], където SE = sqrt( (p1*(1-p1)/n + (p2*(1-p2)/m ) )
+#нормално разпределение, когато tita = mu (не знаем очакването, но знаем дисперсията) -> интервала е [x.cherta - z* sigma/sqrt(n), x.cherta + z* sigma/sqrt(n)], където x.cherta = sum(Xi)/n = mean(xi) (средно аритметично - експериментално очакване)
+#нормално разпределение, когато tita = mu, sigma (не знаем нито очакването, нито дисперсията) -> интервала е [x.cherta - t* s/sqrt(n), x.cherta + t* s/sqrt(n)], където x.cherta = sum(Xi)/n = mean(xi), s = sd(Xi) (експериментално стандартно отклонение), t* = qt(1-alpha, df = n -1)квантил на t-разпределение с n-1 степени на свобода
 
 #-----------------------------------------------------
 
@@ -52,42 +59,54 @@ temp = c(102.5, 101.7, 103.1, 100.9, 100.5, 102.2)
 sd = 1.2
 x=mean(temp)
 alpha=1-0.95
-z=qnorm(1-alpha/2)
+z=qnorm(1-alpha/2) 
 lower=x-z*sd
 upper=x+z*sd
 
 #03
-#1000 случайни нормално разпределени величини N(2,2)
-x = rnorm(n = 1000, mean = 2, sd = sqrt(2))
-alpha=1-0.95
-z=qnorm(1-alpha/2)
-x.cherta=mean(x)
-#N(mu,1) -> formula 10
-x.lower=x.cherta-z*1
-x.upper=x.cherta+z*1
-plot(x)
-lines(x=0:1000,y=rep(x.upper,1001), col='red')
-lines(x=0:1000,y=rep(x.lower,1001), col='blue')
+#1000 случайни нормално разпределени величини
+test.mu = function(mu, sigma.sq) {
+  x = rnorm(n = 1000, mean = mu, sd = sqrt(sigma.sq))
+  alpha=1-0.95
+  z=qnorm(1-alpha/2)
+  x.cherta=mean(x)
+  #N(mu,1) -> formula 11
+  x.lower=x.cherta-z*1
+  x.upper=x.cherta+z*1
+  plot(x)
+  lines(x=0:1000,y=rep(x.upper,1001), col='red')
+  lines(x=0:1000,y=rep(x.lower,1001), col='blue')
+  
+  x.length=length(x[x>=x.lower&x<=x.upper]) #броят на стойностите влизащи в доверителния интервал
+  print(x.length)
+  print(mean(x[x>=x.lower&x<=x.upper])) #средното на броят стойностите, влизащи в интервала
+  x.length/1000 #вероятността някоя от случайните величини да е вътре в доверителния интервал
+}
 
-x.l=length(x[x>=lower&x<=x.upper])
-mean(x[x>=lower&x<=x.upper])
-x.pro=x.l/10
-
+#N(2,2)
+test.mu(2,2)
 #N(2,1)
-y=rnorm(n=1000, mean=2, sqrt(1))
-alpha=1-0.95
-z=qnorm(1-alpha/2)
-y.cherta=mean(y)
-#N(mu,1) -> formula 10
-y.lower=y.cherta-z*1
-y.upper=y.cherta+z*1
-plot(y)
-lines(x=0:1000,y=rep(y.upper,1001), col='red')
-lines(x=0:1000,y=rep(y.lower,1001), col='blue')
-
-y.l=length(y[y>=lower&y<=y.upper])
-mean(y[y>=lower&y<=y.upper])
-y.pro=y.l/10
+test.mu(2,1)
 
 #04
+#1000 случайни нормално разпределени величини
+test.mu.sigma = function(mu, sigma.sq) {
+  x = rnorm(n = 1000, mean = mu, sd = sqrt(sigma.sq))
+  x.cherta = mean(x)
+  s = sd(x)
+  gama = 0.95
+  t = qt(gama, df = 1000 - 1) #квантил на t-разпределение с n-1 степени на свобода
+  #N(mu,sigma^2) -> формула 12
+  x.lower = x.cherta - t*s
+  x.upper = x.cherta + t*s
+  plot(x)
+  lines(x=0:1000,y=rep(x.upper,1001), col='red')
+  lines(x=0:1000,y=rep(x.lower,1001), col='blue')
+  
+  x.length=length(x[x>=x.lower&x<=x.upper]) #броят на стойностите влизащи в доверителния интервал
+  print(x.length)
+  print(mean(x[x>=x.lower&x<=x.upper])) #средното на броят стойностите, влизащи в интервала
+  x.length/1000 #вероятността някоя от случайните величини да е вътре в доверителния интервал
+}
+
 
